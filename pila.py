77f43f060efcb5df7faf689788dc872d9e30be8a
Gabriel Ramos <gabrieljhramos@gmail.com>
2021-04-27 19:38:28 -0300

    Actualizado cola

--- pila.py
+++ pila.py
@@ -15,3 +15,29 @@ class Pila(object):
 
     def tamanio(self):
         return len(self.__elementos)
+
+    def elemento_cima(self):
+        return self.__elementos[-1]
+
+# from random import randint
+
+# pila = Pila()
+# pila_aux = Pila()
+
+# for i in range(0, 10):
+#     pila.apilar(randint(0, 100))
+
+# print('cantidad de elementos', pila.tamanio())
+
+# while(not pila.pila_vacia()):
+#     x = pila.desapilar()
+#     pila_aux.apilar(x)
+#     print(x)
+
+# print()
+# while(not pila_aux.pila_vacia()):
+#     x = pila_aux.desapilar()
+#     pila.apilar(x)
+
+# print()
+# print(pila.tamanio())
