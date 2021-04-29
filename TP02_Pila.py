2932d7b20f0fdd093775d6fcaf45ad75a46111f8
tommy63 <tomas.alaluf63@gmail.com>
2021-04-29 15:53:13 -0300

    ejercicio 16 resuelto

--- TP02_Pila.py
+++ TP02_Pila.py
@@ -68,3 +68,49 @@ def eliminarIesimo(pila : Pila, iesimo : int):
 #             pilaNumeros.apilar(pilaAux.desapilar())
 
 # pilaNumeros.barrido_pila()
+
+
+# Ejercicio 16
+
+pilaCapitulo_V = Pila()
+pilaCapitulo_VII = Pila()
+pila_intersec = Pila()
+
+Capitulo_V = ['yoda','luke','leia','boba fett','darth vader','obi-wan']
+Capitulo_VII = ['yoda','leia','obi-wan','finn','rey','kylo ren']
+
+for cap in Capitulo_V:
+    pilaCapitulo_V.apilar(cap)
+
+
+
+for cap in Capitulo_VII:
+    pilaCapitulo_VII.apilar(cap)
+
+while(not pilaCapitulo_V.pila_vacia()):
+    x = pilaCapitulo_V.elemento_cima()
+
+    while( not pilaCapitulo_VII.pila_vacia()):
+        c = pilaCapitulo_VII.desapilar()
+        if x == c:
+            pila_intersec.apilar(x)
+        
+    for cap in Capitulo_VII:
+        pilaCapitulo_VII.apilar(cap)       
+
+        
+    x = pilaCapitulo_V.desapilar()
+    
+
+pila_intersec.barrido_pila()
+           
+          
+
+
+
+
+
+    
+
+    
+
